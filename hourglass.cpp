aba9e6c45cefd2be056eddd3feb8ad6a1d864fd9
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2022-09-01 20:12:34 +0700

    Update hourglass.cpp

--- hourglass.cpp
+++ hourglass.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main(){
 
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int k = 1; k <= i - 1; k++){
+            cout << " ";
+        }
+        for (int j = 1; j <= 5 - i + 1;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     
     
     return EXIT_SUCCESS;
